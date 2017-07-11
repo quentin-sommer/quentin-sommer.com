d5b238f8f217c166d64ea9df6bc739b9473b7091
Quentin Sommer <sommerquentin67@gmail.com>
2017-07-11 09:12:03 +0200

    Added title and description

--- layouts/Page.js
+++ layouts/Page.js
@@ -1,12 +1,32 @@
 import * as React from 'react'
+import Head from 'next/head'
 import Base from './Base'
 import Container from '../components/Container'
 
-const Page = ({children}) =>
-  <Base>
-    <Container>
-      {children}
-    </Container>
-  </Base>
+const baseTitle = 'Quentin Sommer'
+const getFullTitle = title =>
+  title === baseTitle ? title : `${title} - ${baseTitle}`
 
+const Page = ({title, description, children}) => {
+  const fullTitle = getFullTitle(title)
+  return (
+    <Base>
+      <Head>
+        <title>
+          {fullTitle}
+        </title>
+        <meta name="description" content={description} />
+      </Head>
+      <Container>
+        {children}
+      </Container>
+    </Base>
+  )
+}
+
+Page.defaultProps = {
+  title: baseTitle,
+  description:
+    'Personal website where you can find out about my work, open source projects, and contact info',
+}
 export default Page
