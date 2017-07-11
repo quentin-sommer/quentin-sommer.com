391f1638fecc15a7c280b8be3d952a090a9109df
Quentin Sommer <sommerquentin67@gmail.com>
2017-07-11 09:26:07 +0200

    Switched to self hosted fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,38 @@
 import * as React from 'react'
 import Document, {Head, Main, NextScript} from 'next/document'
-import styled, {ServerStyleSheet} from 'styled-components'
-import {TypographyStyle, GoogleFont} from 'react-typography'
+import {ServerStyleSheet} from 'styled-components'
+import {TypographyStyle} from 'react-typography'
 import typography from '../lib/typography'
 import theme from '../lib/theme'
+
+const fonts = `
+@font-face {
+  font-family: 'Amaranth';
+  font-style: normal;
+  font-weight: 400;
+  src: url('/static/fonts/amaranth-latin-400.eot');
+  src:
+    local('Amaranth Regular '),
+    local('Amaranth-Regular'),
+    url('/static/fonts/amaranth-latin-400.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/amaranth-latin-400.woff2') format('woff2'),
+    url('/static/fonts/amaranth-latin-400.woff') format('woff'),
+    url('/static/fonts/amaranth-latin-400.svg#Amaranth') format('svg');
+}
+@font-face {
+  font-family: 'Amaranth';
+  font-style: normal;
+  font-weight: 700;
+  src: url('/static/fonts/amaranth-latin-700.eot');
+  src:
+    local('Amaranth Bold '),
+    local('Amaranth-Bold'),
+    url('/static/fonts/amaranth-latin-700.eot?#iefix') format('embedded-opentype'),
+    url('/static/fonts/amaranth-latin-700.woff2') format('woff2'),
+    url('/static/fonts/amaranth-latin-700.woff') format('woff'),
+    url('/static/fonts/amaranth-latin-700.svg#Amaranth') format('svg');
+}
+`.replace(/(?:\n|\s)/g, '')
 
 export default class MyDocument extends Document {
   render() {
@@ -16,8 +45,8 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta http-equiv="x-ua-compatible" content="ie=edge" />
           <meta name="theme-color" content={theme.bg} />
+          <style dangerouslySetInnerHTML={{__html: fonts}} />
           <TypographyStyle typography={typography} />
-          <GoogleFont typography={typography} />
           {styleTags}
         </Head>
         <body>
