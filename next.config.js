b3039ca977db22b4f9b1cc991ec382c32e5a4f38
Quentin Sommer <sommerquentin67@gmail.com>
2018-06-16 16:18:28 -0400

    feat: Removed bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const IgnorePlugin = require('webpack').IgnorePlugin
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const path = require('path')
 
 const disableBableLoderCache = config => {
@@ -12,23 +11,8 @@ const disableBableLoderCache = config => {
   })
 }
 
-const injectBundleAnalyzerPlugin = config => {
-  config.plugins.push(
-    new BundleAnalyzerPlugin({
-      // this disable the plugin builtin web server and only outpus to a file
-      analyzerMode: 'disabled',
-      generateStatsFile: true,
-      statsFilename: 'stats.json', // Will be available at `.next/stats.json`
-      // For all options see https://github.com/th0r/webpack-bundle-analyzer#as-plugin
-    })
-  )
-}
-
 module.exports = {
   webpack: (config, {dev}) => {
-    if (!dev) {
-      injectBundleAnalyzerPlugin(config)
-    }
     disableBableLoderCache(config)
     return config
   },
