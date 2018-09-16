cc14e85e2570dda2b441b0f55d6a57160bb3f36c
Quentin Sommer <sommerquentin67@gmail.com>
2018-09-16 12:09:45 +0200

    feat: Added open graph data

--- layouts/Page.js
+++ layouts/Page.js
@@ -6,6 +6,7 @@ import Container from '../components/Container'
 const baseTitle = 'Quentin Sommer'
 const getFullTitle = title =>
   title === baseTitle ? title : `${title} - ${baseTitle}`
+const baseUrl = 'https://www.quentin-sommer.com'
 
 const Page = ({title, description, children, url}) => {
   const fullTitle = getFullTitle(title)
@@ -14,6 +15,16 @@ const Page = ({title, description, children, url}) => {
       <Head>
         <title>{fullTitle}</title>
         <meta name="description" content={description} />
+        <meta property="og:title" content={fullTitle} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`${baseUrl}${url}`} />
+        <meta
+          property="og:image"
+          content={`${baseUrl}/static/background.jpg`}
+        />
+        <meta property="og:description" content={description} />
+        <meta property="og:locale" content="en" />
+        <meta property="og:site_name" content="Quentin Sommer" />
       </Head>
       <Container>{children}</Container>
     </Base>
